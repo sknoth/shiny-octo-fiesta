2d40a05fbed1fa8080879a0518385a3e4f3e7404
Stefanie Knoth <Stefanie Knoth>
2018-03-08 15:13:19 +0100

    bla

--- backend/prepareDiagnosisData.py
+++ backend/prepareDiagnosisData.py
@@ -6,16 +6,11 @@ workbook = xlrd.open_workbook('../data/AcuteGlucose 2014 11 13 Diagnoses and car
 sheet = workbook.sheet_by_index(1)
 
 dDict = {}
-
-#diagnosesForAnnotation = []
-
-
-
+dKeys = []
 
 for row in range(2, sheet.nrows):
     
     diagnosis = sheet.row(row)[7].value
-    
     
     s = []
     dLen = len(diagnosis)
@@ -24,33 +19,32 @@ for row in range(2, sheet.nrows):
     i = 1
     while i <= dLen:
         s.append(diagnosis[0:i])
-        i += 1        
-    
+        i += 1      
+     
+    # added all part diagnosis keys for later referencing    
+    dKeys.extend(s)
+
     # initialize tree stucture
-    c = 0
     if not dDict.get(s[0]):
         dDict[s[0]] = {}
-        c += 1
         
     if not dDict[s[0]].get(s[1]):
         dDict[s[0]][s[1]] = {}
-        c += 1
         
     if not dDict[s[0]][s[1]].get(s[2]):
         dDict[s[0]][s[1]][s[2]] = {}
-        c += 1
     
     if not dDict[s[0]][s[1]][s[2]].get(s[3]):
         dDict[s[0]][s[1]][s[2]][s[3]+'+'] = {}
         dDict[s[0]][s[1]][s[2]][s[3]] = 1
-        c += 1
+        dKeys.append(s[3]+'+')
         
     if dLen > 4 and not dDict[s[0]][s[1]][s[2]][s[3]+'+'].get(s[4]):
         dDict[s[0]][s[1]][s[2]][s[3]+'+'][s[4]] = 1
         
-    
     # populate tree
     dDict[s[0]][s[1]][s[2]][s[3]] += 1
+    
     if dLen > 4:
         dDict[s[0]][s[1]][s[2]][s[3]+'+'][s[4]] += 1
     
@@ -62,3 +56,7 @@ output = {
 with open('result.json', 'w') as fp:
     json.dump(output, fp)
     
+dKeys = list(set(dKeys))
+
+with open('dKeys.json','w') as fp:
+    json.dump(dKeys, fp)
